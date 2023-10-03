ed1e644d35c9803908cbbdbe1eef10523d95e03e
alextirgard00n <tirgardoon@gmail.com>
2023-10-03 13:44:22 -1000

    test code for js

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,1 @@
-console.log("hello world"),console.log("testing watch function");
+console.log("hello world"),console.log("testing watch function"),console.log("testing github pages");
--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 console.log("hello world");
 console.log("testing watch function");
+console.log("testing github pages");
